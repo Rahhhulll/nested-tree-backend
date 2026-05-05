06a0d9fe886565fe5749ff6faec8106b159ad0d7
Rahhhulll <rkrahulrk651@gmail.com>
2026-05-05 14:20:10 +0530

    fix mongo connection

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ app.use(express.json());
 app.use('/api', memberRoutes);
 
 // ✅ MongoDB Atlas connection (ENV se)
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected ✅'))
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected ✅'))
+.catch((err) => console.log(err));
 
 // test route
 app.get('/', (req, res) => {
