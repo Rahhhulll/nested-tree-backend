2d0a84d592d035e065a639e465ec85682d13e35c
Rahhhulll <rkrahulrk651@gmail.com>
2026-05-05 13:42:29 +0530

    fixed mongo + port

--- server.js
+++ server.js
@@ -9,16 +9,18 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', memberRoutes);
 
-// 🔥 MongoDB connection
-mongoose.connect('mongodb://127.0.0.1:27017/nestedtree')
+// ✅ MongoDB Atlas connection (ENV se)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected ✅'))
   .catch((err) => console.log(err));
 
+// test route
 app.get('/', (req, res) => {
   res.send('Server running 🚀');
 });
 
-const PORT = 5000;
+// ✅ Dynamic PORT
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
